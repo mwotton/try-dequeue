27ed3d2f9414727fa884161b66403d0f901d8e11
William Rusnack <williamrusnack@gmail.com>
2018-05-23 08:29:51 -0400

    added Dequeue example

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,12 @@
 module Main where
 
+-- Docs: http://hackage.haskell.org/package/dequeue-0.1.12/docs/Data-Dequeue.html
 import Data.Dequeue as D
 
 main :: IO ()
 main = do
---  let a = D.empty
---  let a' = a.pushFront 1
---  let b = D.takeFront 1 a'
-  let b = Just 1
+
+  let a = D.empty
+  let a' = D.pushFront a 'a'
+  let b = D.first a'
   print b
